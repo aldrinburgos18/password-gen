5e36f94c82f2219ee4e27f11a24b2d1dff8cd8f1
Aldrin Burgos <burgos.aldrin@gmail.com>
2024-04-25 18:57:46 -0500

    fix bug during recursion

--- script.js
+++ script.js
@@ -3,38 +3,45 @@ const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const specialChars = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
 
-var password = "";
-
-
 const generatePassword = function() {
+  //variable to store selected characters
+  password = '';
   var selectedChars = [];
+
   var charNum = prompt('How many characters do you want? Please choose between 8-128');
 
   if(charNum >= 8 && charNum <= 128) {
     var confirmLower = confirm('Do you want lower case characters on your password?');
     if (confirmLower) {
-      selectedChars += lowerCase;
+      selectedChars.push(...lowerCase);
     }
 
     var confirmUpper = confirm('Do you want upper case characters on your password?');
     if(confirmUpper) {
-      selectedChars += upperCase;
+      selectedChars.push(...upperCase);
     }
 
     var confirmNumbers = confirm('Do you want numbers on your password?');
     if(confirmNumbers) {
-      selectedChars += numbers;
+      selectedChars.push(...numbers);
     } 
 
     var confirmSpecials = confirm('Do you want special characters on your password? (!, @, #, etc.)' );
     if(confirmSpecials) {
-      selectedChars += specialChars;
+      selectedChars.push(...specialChars);
     }
 
-    console.log(selectedChars);
+    if(!confirmLower && !confirmUpper && !confirmNumbers && !confirmSpecials) {
+      alert('Please select at least 1 type of character.');
+      return generatePassword();
+    }
+
+    //randomize selected characters
+    selectedChars = selectedChars.sort(() => {return 0.5 - Math.random()}).splice(0, charNum);
+    return password = selectedChars.join('')
   } else { 
-    alert("Please enter a number between 8-128.")
-    generatePassword()
+    alert("Please enter a number between 8-128.");
+    return generatePassword();
   }
 }
 
@@ -46,8 +53,8 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  console.log(password)
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
